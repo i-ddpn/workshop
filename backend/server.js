c7fb21401f0c35202b11fa9a2047492438ded560
Ivan M <kob7mi@gmail.com>
2021-06-09 13:11:40 +0300

    DB initialize 2

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import services from './sample_data/services.js'
+
+import serviceRoutes from './routes/serviceRoutes.js'
 
 dotenv.config()
 
 connectDB()
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send
-})
-
-app.get('/api/services', (req, res) => {
-  res.json(services)
-})
-
-app.get('/api/services/:id', (req, res) => {
-  const service = services.find((s) => s._id === req.params.id)
-  res.json(service)
-})
+app.use('/api/services', serviceRoutes)
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, console.log('Server running'))
